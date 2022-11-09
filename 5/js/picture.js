964b0e2f757228700f805cf1a59cfc5d277e31f5
Keks <keks@htmlacademy.ru>
2022-11-10 01:14:35 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/picture.js
@@ -0,0 +1,22 @@
+import {createPhotos} from './data.js';
+
+const pictureConteiner = document.querySelector('.pictures');
+
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const simularPhotos = createPhotos();
+
+const pictureFragment = document.createDocumentFragment();
+
+simularPhotos.forEach(({url, likes, comments}) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureFragment.appendChild(pictureElement);
+});
+
+pictureConteiner.appendChild(pictureFragment);
+

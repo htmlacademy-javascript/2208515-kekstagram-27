864972a5f81b2b9c0e83c7ac6d17a037e69a4fbd
Keks <keks@htmlacademy.ru>
2022-11-11 16:47:02 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/picture.js
+++ 5/js/picture.js
@@ -1,20 +1,16 @@
-import {createPhotos} from './data.js';
-
 const pictureConteiner = document.querySelector('.pictures');
 
 const pictureTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-const simularPhotos = createPhotos();
-
-const createPictures = () => {
+const createPictures = (pictures) => {
   const pictureFragment = document.createDocumentFragment();
-  simularPhotos.forEach(({url, likes, comments}) => {
+  pictures.forEach((picture) => {
     const pictureElement = pictureTemplate.cloneNode(true);
-    pictureElement.querySelector('.picture__img').src = url;
-    pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    pictureElement.querySelector('.picture__img').src = picture.url;
+    pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+    pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
     pictureFragment.appendChild(pictureElement);
   });
   pictureConteiner.appendChild(pictureFragment);
